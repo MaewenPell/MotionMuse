79951815f59812ab5975224d63df5bd907a8568c
Maewen Pelletier <mpelletier@tihive.onmicrosoft.com>
2024-03-09 17:25:05 +0100

    patch test

--- src/app/shared/services/strava.service.spec.ts
+++ src/app/shared/services/strava.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MessageService } from 'primeng/api';
 import { StravaService } from './strava.service';
 
 describe('StravaService', () => {
@@ -8,6 +9,7 @@ describe('StravaService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      providers: [MessageService],
     });
     service = TestBed.inject(StravaService);
   });
